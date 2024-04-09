bab79f89a63a9ea2b7830097514a80e10117a8d7
Md Tanvir Hossain <md.tanvir.hossai.tusher>
2024-04-10 01:26:51 +0600

    one page pdf done

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -13,9 +13,10 @@ def extract_data_from_pdf(pdf_file):
         reader = PyPDF2.PdfReader(file)
         text = ""
         for page_num in range(len(reader.pages)):
-            text += reader.pages[page_num].extract_text()
 
-        print(text, end=" ===\n")
+            text += reader.pages[page_num].extract_text()
+            print(page_num, end="-------------------------------\n")
+            print(text, end=" ===\n")
 
     lines = text.split("\n")
 
@@ -38,20 +39,6 @@ def extract_data_from_pdf(pdf_file):
         pieces = match.group(4)
         service = match.group(5)
         box_weight = match.group(6).split()[1]
-        # ACCOUNT NUMBER CUSTOMER Origin Destination Pieces SERVICE Box Weight
-        keys = [
-            "ACCOUNT NUMBER",
-            "CUSTOMER",
-            "Destination",
-            "Pieces",
-            "SERVICE",
-            "Box Weight",
-        ]
-        values = [account_number, customer, destination, pieces, service, box_weight]
-
-        data = {key: value for key, value in zip(keys, values)}
-
-        print(data)
 
         # print(
         #     account_number,
@@ -65,32 +52,28 @@ def extract_data_from_pdf(pdf_file):
 
         # return account_number, customer, destination, pieces, service, box_weight
     else:
-        print("nai")
+        print("No value found!")
         # return None
 
     company = lines[5].split()
-
     if len(company) == 3:
         senders_company = company[0]
         recipients_company = company[1]
     elif len(company) >= 5:
         senders_company = " ".join(company[:2])
         recipients_company = " ".join(company[2:4])
-
-    print(senders_company)
-    print(recipients_company)
+    # print(senders_company)
+    # print(recipients_company)
 
     names = lines[7].split()
-
     if len(names) == 3:
         senders_name = names[0]
         recipients_name = names[1]
     elif len(names) >= 5:
         senders_name = " ".join(names[:2])
         recipients_name = " ".join(names[2:4])
-
-    print(senders_name)
-    print(recipients_name)
+    # print(senders_name)
+    # print(recipients_name)
 
     # Assuming address_line contains '1 GULDEHRA, 54 KURUKSHETRA 12 ASHFORD AVENUE, HAYES 13.80'
     # address_line = "1 GULDEHRA edd, 54 KURUKSHETRA ds 12 ASHFORD AVENUE, HAYES 13.80"
@@ -126,41 +109,79 @@ def extract_data_from_pdf(pdf_file):
         f"{recipients_address[::-1]},{senders_address_last_part}".strip()
     )
     senders_address = (
-        address[0] + address[1][::-1][first_address_second_half_idx + 1 :][::-1]
+        f"{address[0]},{address[1][::-1][first_address_second_half_idx + 1 :][::-1]}"
     )
 
-    print(senders_address)
-    print(recipients_address)
-    # print(recipients_address[::-1] + "," + senders_address_last_part)
-    # print(address[0] + address[1][::-1][idx + 1 :][::-1])
+    # print(senders_address)
+    # print(recipients_address)
 
-    # senders_address = address[0] + "," +
+    chargeble_weight = lines[11]
 
-    # # Find the second address
-    # second_address_match = re.search(
-    #     r"(?<=\d\.\d{2}\s)(.*?)(?=\s\d)", address_line[::-1]
-    # )
-    # if second_address_match:
-    #     second_address = second_address_match.group(0).strip()
-    #     print("Second Address:", second_address[::-1])
+    print(lines[12].split())
 
-    # Find the first address
-    # first_address_match = re.search(r"(\d+\s+\S+\s*)*(?=\s+\d+\s)", address_line)
-    # if first_address_match:
-    #     first_address = first_address_match.group(0).strip()
-    #     print("First Address:", first_address)
+    pins_tel = lines[12].split()
+    if lines[12][9].lower() == "t":
+        senders_pin = lines[13].split(",")[0]
+        senders_telephone = pins_tel[3]
+        recipients_pin = pins_tel[5]
+        recipients_telephone = pins_tel[8]
+    else:
+        senders_pin = pins_tel[1]
+        senders_telephone = pins_tel[4]
+        recipients_pin = pins_tel[6]
+        recipients_telephone = pins_tel[9]
 
-    # address_line_without_first = re.sub(
-    #     re.escape(first_address), "", address_line, count=1
-    # )
-    # print("Address Line without First Address:", address_line_without_first)
+    goods_description_and_booking_date = lines[15].split("-")
+    # print(goods_description_and_booking_date, end=" ,,, \n")
+    goods_description = goods_description_and_booking_date[0][:-4]
+    booking_date = goods_description_and_booking_date[1].split()[1]
 
-    # second_address_match = re.search(
-    #     r"(\d+\s+\S+\s*)*(?=\s+\d+\s)", address_line_without_first
-    # )
-    # if second_address_match:
-    #     second_address = second_address_match.group(0).strip()
-    #     print("Second Address:", second_address)
+    keys = [
+        "ACCOUNT NUMBER",
+        "CUSTOMER",
+        "Destination",
+        "Pieces",
+        "SERVICE",
+        "Box Weight",
+        "SENDER'S COMPANY",
+        "RECIPIENT'S COMPANY",
+        "SENDER'S NAME",
+        "RECIPIENT'S NAME",
+        "SENDER'S ADDRESS",
+        "RECIPIENT'S ADDRESS",
+        "CHARGEABLE WT.",
+        "SENDER'S PinCode",
+        "SENDER'S TEL NO.",
+        "RECIPIENT'S PinCode",
+        "RECIPIENT'S TEL NO.",
+        "DESCRIPTION OF GOODS",
+        "BOOKING DATE",
+    ]
+    values = [
+        account_number,
+        customer,
+        destination,
+        pieces,
+        service,
+        box_weight,
+        senders_company,
+        recipients_company,
+        senders_name,
+        recipients_name,
+        senders_address,
+        recipients_address,
+        chargeble_weight,
+        senders_pin,
+        senders_telephone,
+        recipients_pin,
+        recipients_telephone,
+        goods_description,
+        booking_date,
+    ]
+
+    data = {key: value for key, value in zip(keys, values)}
+
+    # print(data)
 
     # # Extract data using regex patterns
     # for key, pattern in patterns.items():
@@ -174,7 +195,7 @@ def extract_data_from_pdf(pdf_file):
 def main():
     pdf_files = ["1712647796-pdf2_A1107146-(1).pdf"]
 
-    extract_data_from_pdf("1712647796-pdf2_A1107146-(1).pdf")
+    extract_data_from_pdf("1712647796-pdf2_A1107144-(1).pdf")
 
     # for pdf_file in pdf_files:
     #     print(f"Extracting data from {pdf_file}:")
